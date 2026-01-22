e3b3527a1635e714ee0fd01bd41d698f44429120
Sanoj-Omli <kumarisupriyasoni96@gmail.com>
2026-01-22 22:39:01 +0530

    add Sidebar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -11,7 +11,10 @@ import FlexBetween from "components/FlexBetween";
 import { useDispatch } from "react-redux";
 import { setMode } from "assets/state";
 
-const Navbar = () => {
+const Navbar = ({
+  isSidebarOpen,
+  setIsSidebarOpen
+}) => {
   const dispatch = useDispatch();
   const theme = useTheme();
 
@@ -26,7 +29,7 @@ const Navbar = () => {
       <Toolbar sx={{ justifyContent: "space-between" }}>
         {/* LEFT SIDE */}
         <FlexBetween>
-          <IconButton onClick={() => console.log("open/close sidebar")}>
+          <IconButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
             <MenuIcon />
           </IconButton>
 

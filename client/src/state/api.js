9ce1866e4e4b51dfcde913c1e6ebfd0e936c1c81
Sanoj-Omli <kumarisupriyasoni96@gmail.com>
2026-01-28 20:05:28 +0530

    added Overview section of Sales

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -3,12 +3,12 @@ import { createApi,fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
     baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL}),
     reducerPath: "adminApi",
-    tagTypes: ["User", "Products","Customers","Transactions", "Geography"], 
+    tagTypes: ["User", "Products","Customers","Transactions", "Geography", "Sales"], 
     endpoints: (build) => ({
         getUser: build.query({
              query: (id) => `general/user/${id}`,
              providesTags: ["User"],
-        }),
+        }),   
         getProducts: build.query({
             query: ()=> "client/products",
             providesTags: ["Products"],
@@ -29,10 +29,14 @@ export const api = createApi({
             query: () => "client/geography",
             providesTags: ["Geoogrphy"]
         }),
+        getSales: build.query({
+            query: () => "sales/sales",
+            providesTags: ["Sales"]
+        })
     }),
 });
  
 export const {
     useGetUserQuery, useGetProductsQuery, useGetCustomersQuery , useGetTransactionsQuery
-    , useGetGeographyQuery
+    , useGetGeographyQuery, useGetSalesQuery
 } = api;
--- server/index.js
+++ server/index.js
@@ -15,7 +15,14 @@ import User from "./models/User.js";
 import Product from "./models/Product.js";
 import ProductStat from "./models/ProductStat.js";
 import Transaction from "./models/Transaction.js";
-import { dataUser , dataProduct, dataProductStat, dataTransaction } from "./data/index.js"
+import OverallStat from "./models/OverallStat.js";
+import { 
+  dataUser , 
+  dataProduct, 
+  dataProductStat, 
+  dataTransaction ,
+  dataOverallStat,
+} from "./data/index.js"
 
 /* CONFIG */
 dotenv.config();
@@ -63,6 +70,7 @@ mongoose.connect(process.env.MONGO_URL)
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
      
      /* ONLY ADD DATA ONE TIME */
+     //OverallStat.insertMany(dataOverallStat);
     //User.insertMany(dataUser);
     //Product.insertMany(dataProduct);
     //Transaction.insertMany(dataTransaction);
